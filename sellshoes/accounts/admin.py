59cc1419f45575adc7064695985865df551d6a49
Prince Chime <chimeprince92@gmail.com>
2024-12-24 17:33:21 +0000

    removed password1 and 2 from the add_fieldset, updated the installed app section of the settings.py file

--- sellshoes/accounts/admin.py
+++ sellshoes/accounts/admin.py
@@ -25,7 +25,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser'),
+            'fields': ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser'),
         }),
     )
     
