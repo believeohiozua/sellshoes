fe63c9415513ce42ffaa451c6278271d47736098
Prince Chime <chimeprince92@gmail.com>
2024-12-24 16:12:36 +0000

    Created the admin interface for custome user model in the admin.py file

--- sellshoes/accounts/admin.py
+++ sellshoes/accounts/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
 from .models import CustomUser
 
-# Register your models here.
-admin.site.register(CustomUser)
+
+
+class CustomUserAdmin(UserAdmin):
+    # The forms to add and change user instances will come from UserAdmin
+    model = CustomUser
+
+    # Fields to be displayed in the admin interface
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+
+    # Fields to be used in the admin user edit page
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+    )
+
+    # Fields to be used in the admin user creation form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser'),
+        }),
+    )
+    
+    filter_horizontal = ()
+
+    # Fields for search functionality
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+# Register the custom user and admin
+admin.site.register(CustomUser, CustomUserAdmin)
 
