4756a4d8b084587cfcfef37817ce2e26bcc7547d
Prince Chime <chimeprince92@gmail.com>
2024-12-27 11:52:06 +0000

    added the api documentation setup - task 003

--- sellshoes/sellshoes/urls.py
+++ sellshoes/sellshoes/urls.py
@@ -17,6 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SellShoes API",
+        default_version="v1",
+        description="An API for selling shoes",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="ups.chime@gmail.com", name="Prince Chime"),
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("test/swagger.json", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-json"),
 ]
