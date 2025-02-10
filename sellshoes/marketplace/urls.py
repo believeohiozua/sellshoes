108b728fd206f6fe9f22b54c27dccf97f5d6a704
Prince Chime <chimeprince92@gmail.com>
2025-02-10 16:54:46 +0000

    corrected the marketplace url

--- sellshoes/marketplace/urls.py
+++ sellshoes/marketplace/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProductListAPIView
 
 urlpatterns = [
-    path('products', ProductListAPIView.as_view(), name='product-list'),
+    path('products/', ProductListAPIView.as_view(), name='product-list'),
 ]
